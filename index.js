a5aece2d41fd3130bd3665558c65dc60863dc214
Jostyn Muentes <jostynroca@gmail.com>
2026-07-20 14:49:27 -0500

    Nombre

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const PORT = process.env.PORT || 3000;
 
 // Cambia este mensaje y haz push a main para ver el despliegue automático en acción
 const MENSAJE = 'Version 1 - Despliegue inicial';
+const nombre1 = 'Jostyn Fabricio Muentes Roca'
 
 app.get('/', (req, res) => {
   res.send(`
     <html>
       <body style="font-family: sans-serif; text-align: center; margin-top: 80px;">
         <h1>${MENSAJE}</h1>
+        <h2>${nombre1}</h2>
         <p>Desplegado: ${new Date().toISOString()}</p>
       </body>
     </html>
